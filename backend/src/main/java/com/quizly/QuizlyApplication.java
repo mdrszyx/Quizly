1e19c34a0c9de94f593997a3288581af3bba6e69
mdrszyx <mdrszyx@gmail.com>
2024-12-19 00:38:57 +0700

    setup backend

---
+++ backend/src/main/java/com/quizly/QuizlyApplication.java
@@ -0,0 +1,13 @@
+package com.quizly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizlyApplication.class, args);
+	}
+
+}
